22f4d6c933e837ba753839038ef60a9edef3da4f
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-24 16:00:06 +0530

    Create addnum.c

---
+++ addnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int n,k,p;
+ printf("enter two numbers n and k");
+ scanf("%d %d",&n,&k);
+ if(n==0 || k==0)
+ {
+   p=n+k;
+   }
+   printf("the value of add of two numbers is"%d",p);
+   }
