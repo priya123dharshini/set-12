0464b4e17dc518d77d83313c231090d8751bc245
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-24 16:22:34 +0530

    Create repitions.c

---
+++ repitions.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+ int num,k,n[i],count=0;
+ printf("enter the total numbers");
+ scanf("%d",&num);
+ printf("enter the k value");
+ scanf("%d",&k);
+ printf("enter the numbers");
+ for(i=0;i<num;i++)
+ {
+  scanf("%d",&n[i]);
+  }
+printf("%d",n[i]);
+for(i=0;i<n[i];i++)
+{
+ if(n[i]==k)
+ {  
+ count++;
+ printf("%d",count);
+ }}
+ else
+{
+ printf("the number is not present");
+ }
