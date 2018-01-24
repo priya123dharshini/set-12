4c6c6a0bccf75a7d5777c032556baa70e67ac335
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-24 16:17:24 +0530

    Create kexixts.c

---
+++ kexixts.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+ int num,k,n[i];
+ printf("enter the total numbers");
+ scanf("%d",&num);
+ printf("enter the k value");
+ scanf("%d",&k);
+ printf("enter the numbers");
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&n[i]);
+  }
+printf("%d",n[i]);
+if(n[i]===k)
+{
+ printf("the number is present");
+ }
+ else
+{
+ printf("the number is not present");
+ }
+ }
