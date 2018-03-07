3d98748e5fb30c040113b2c8c2883aebd537e122
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-03-07 15:42:34 +0530

    Create 115.c

---
+++ 115.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#define MAX 1000
+void main()
+{
+    int N[MAX],K[MAX],n,min;
+    int i,j;
+    printf("ENTER THE ARRAY SIZE");
+    scanf("%d",&n);
+    printf("ENTER VALUE OF N");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&N[i]);
+    }
+    printf("ENTER VALUE OF K");
+    for(j=0;j<n;j++)
+    {
+        scanf("%d",&K[j]);
+    }
+    min=K[0];
+  for(j=1;j<n;j++)
+  {
+    if(K[j]<min)
+    min=K[j];
+  }
+ 
+printf("SMALLEST NUMBER IN K:\t%d",min);
+}
